28154ed64eb8611d445aedf8e6a5795a0db65094
Suyash <singhsuyash012@gmail.com>
2025-08-09 00:11:19 +0530

    Project structure

---
+++ RecreatingUI/RecreatingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecreatingUIApp.swift
+//  RecreatingUI
+//
+//  Created by Suyash Singh on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RecreatingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecreatingUITests/RecreatingUITests.swift
@@ -0,0 +1,17 @@
+//
+//  RecreatingUITests.swift
+//  RecreatingUITests
+//
+//  Created by Suyash Singh on 09/08/25.
+//
+
+import Testing
+@testable import RecreatingUI
+
+struct RecreatingUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
